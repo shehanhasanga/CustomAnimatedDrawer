5dec9e1028a966219ce89f55aa31cd23be7f9782
shehanhasanga <shehanhasanga.15@cse.mrt.ac.lk>
2022-02-11 08:53:03 +0530

    Initial Commit

---
+++ AnimatedDrawer/AnimatedDrawerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedDrawerApp.swift
+//  AnimatedDrawer
+//
+//  Created by shehan karunarathna on 2022-02-11.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedDrawerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
